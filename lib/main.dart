7cb4a133e0fe392634f1864386cd293a06ee0b51
jinyoung <danroom85@gmail.com>
2023-01-27 21:40:35 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: const Padding(
+        padding: EdgeInsets.all(Sizes.size14),
+        child: SignUpScreen(),
+      ),
+    );
+  }
+}
