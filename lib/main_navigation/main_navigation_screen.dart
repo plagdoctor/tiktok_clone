962cce5351f18afc97481858db96aede7f0fdbca
jinyoung <danroom85@gmail.com>
2023-02-11 14:27:36 +0900

    add expanded widgets nav_tab

--- lib/main_navigation/main_navigation_screen.dart
+++ lib/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -41,25 +41,37 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
-        child: Row(
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                FaIcon(
-                  FontAwesomeIcons.house,
-                  color: Colors.white,
-                ),
-                Gaps.v5,
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: 'Home',
+                icon: FontAwesomeIcons.house,
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: 'Search',
+                icon: FontAwesomeIcons.magnifyingGlass,
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: 'Inbox',
+                icon: FontAwesomeIcons.message,
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: 'User',
+                icon: FontAwesomeIcons.user,
+                isSelected: _selectedIndex == 4,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
         ),
       ),
     );
