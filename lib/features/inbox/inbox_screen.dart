88a1b66b191df39f9d638e6507d7ae2225afee99
jinyoung <danroom85@gmail.com>
2023-03-24 22:30:41 +0900

    10.3 onDismissed

--- lib/features/inbox/inbox_screen.dart
+++ lib/features/inbox/inbox_screen.dart
@@ -1,91 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/inbox/activity_screen.dart';
 
-class InboxScreen extends StatelessWidget {
-  const InboxScreen({super.key});
+class ActivityScreen extends StatefulWidget {
+  const ActivityScreen({super.key});
 
-  void _onDmPressed() {}
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
 
-  void _onActivityTap(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => const ActivityScreen(),
-      ),
-    );
+class _ActivityScreenState extends State<ActivityScreen> {
+  final List<String> _notifications = List.generate(20, (index) => "${index}h");
+
+  void _onDismissed(String notification) {
+    _notifications.remove(notification);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    print(_notifications);
     return Scaffold(
       appBar: AppBar(
-        elevation: 1,
-        title: const Text('Inbox'),
-        actions: [
-          IconButton(
-            onPressed: _onDmPressed,
-            icon: const FaIcon(
-              FontAwesomeIcons.paperPlane,
-              size: Sizes.size20,
-            ),
-          ),
-        ],
+        title: const Text("All activity"),
       ),
       body: ListView(
         children: [
-          ListTile(
-            onTap: () => _onActivityTap(context),
-            title: const Text(
-              'Activity',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: Sizes.size16,
-              ),
+          Gaps.v14,
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size12,
             ),
-            trailing: const FaIcon(
-              FontAwesomeIcons.chevronRight,
-              size: Sizes.size14,
-              color: Colors.black,
-            ),
-          ),
-          Container(
-            height: Sizes.size1,
-            color: Colors.grey.shade200,
-          ),
-          ListTile(
-            leading: Container(
-              width: Sizes.size52,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
-              child: const Center(
-                child: FaIcon(
-                  FontAwesomeIcons.users,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            title: const Text(
-              'New followers',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: Sizes.size16,
-              ),
-            ),
-            subtitle: const Text(
-              'Messages from followers will apear here',
+            child: Text(
+              'New',
               style: TextStyle(
                 fontSize: Sizes.size14,
+                color: Colors.grey.shade500,
               ),
             ),
-            trailing: const FaIcon(
-              FontAwesomeIcons.chevronRight,
-              size: Sizes.size14,
-              color: Colors.black,
-            ),
-          )
+          ),
+          Gaps.v14,
+          for (var notification in _notifications)
+            Dismissible(
+              key: Key(notification),
+              onDismissed: (direction) => _onDismissed(notification),
+              background: Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.green,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    left: Sizes.size10,
+                  ),
+                  child: FaIcon(
+                    FontAwesomeIcons.checkDouble,
+                    color: Colors.white,
+                    size: Sizes.size24,
+                  ),
+                ),
+              ),
+              secondaryBackground: Container(
+                alignment: Alignment.centerRight,
+                color: Colors.red,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    right: Sizes.size10,
+                  ),
+                  child: FaIcon(
+                    FontAwesomeIcons.trashCan,
+                    color: Colors.white,
+                    size: Sizes.size24,
+                  ),
+                ),
+              ),
+              child: ListTile(
+                minVerticalPadding: Sizes.size16,
+                leading: Container(
+                  width: Sizes.size52,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: Sizes.size1,
+                    ),
+                  ),
+                  child: const Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.bell,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                title: RichText(
+                  text: TextSpan(
+                    text: "Account updates:",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                      fontSize: Sizes.size16,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: " Upload longer videos",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " $notification",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                trailing: const FaIcon(
+                  FontAwesomeIcons.chevronRight,
+                  size: Sizes.size16,
+                ),
+              ),
+            )
         ],
       ),
     );
