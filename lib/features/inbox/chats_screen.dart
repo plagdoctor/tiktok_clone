d50df07360b6d5a2840ecd8f719908855028d3e0
jinyoung <danroom85@gmail.com>
2023-03-24 22:58:07 +0900

    11.1 direct Messages

---
+++ lib/features/inbox/chats_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
